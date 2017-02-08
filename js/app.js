1ed12e2c835be27a1fab543afc1aac8ce4273cec
isabelzv <isabel.s.marriott@gmail.com>
2017-02-07 21:32:51 -0700

    Add sprite class constructor

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
-//TODO define sprite
+var heartImage = new Image();
+coinImage.src = "images/bloodSprite.png";
+
+//grab canvas
+var canvas = document.getElementById("coinAnimation");
+canvas.width = 100;
+canvas.height = 100;
+
+//define sprite object
+var sprite = function(options) {
+	var that = {};
+
+	that.context = options.context;
+	that.width = options.width;
+	that.height = options.height;
+	that.image = options.image;
+
+	return that;
+}
 
 //TODO create sprite object using blood strip
 
-//TODO grab canvas
+
 
 //TODO draw image function
 
